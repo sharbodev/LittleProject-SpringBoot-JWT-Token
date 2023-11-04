498a4dfcd4eaacebe13440ad778f59b0f9bad928
BoburFlexer <bobursakirov2008@gmail.com>
2023-11-04 18:01:01 +0600

    fixed showing password

--- src/main/java/com/example/littleprojectspringbootjwttoken/service/auth/Auth.java
+++ src/main/java/com/example/littleprojectspringbootjwttoken/service/auth/Auth.java
@@ -15,12 +15,12 @@ public class Auth {
     private final JwtUtils jwtUtils;
     private final PasswordEncoder passwordEncoder;
 
-    public void save(String fullName, String email, String password, Role role) {
+    public void save(String email, String password, String fullName, Role role) {
         User user = new User();
         user.setEmail(email);
-        user.setRole(role);
+        user.setPassword(passwordEncoder.encode(password));
         user.setFullName(fullName);
-        user.setPassword(password);
+        user.setRole(role);
         if (userRepository.existsByEmail(email)){
             throw new RuntimeException("user exists email");
         }
